cadfb09ca17467e0280769d607148ee04aa39cc4
Alayne1 <alaynelxr@gmail.com>
2023-02-11 16:04:32 +0800

    added part 2 of SPS game with states and user name

--- script.js
+++ script.js
@@ -2,6 +2,13 @@
 // Input Scissors, Paper, or Stone
 // Output is the result of Win, Lose, Draw or input validation of "invalid"
 
+// Set states
+
+var countOfWins = 0;
+var countOfRounds = 0;
+var userName = "";
+var currentGameMode = "waiting for user name";
+
 // Generate a random integer from 1 to 3
 
 var generateRandomNumber = function () {
@@ -28,31 +35,48 @@ var generateGuess = function () {
   return "Oops! There was a mistake!";
 };
 
-var main = function (playerGuess) {
-  var computerGuess = generateGuess();
-  console.log("This is what the computer played: ", computerGuess);
-  var outcomeOutput = `Your guess: ${playerGuess}. Computer's guess: ${computerGuess}.`;
-  console.log("this is players guess", playerGuess);
-  if (computerGuess == playerGuess) {
-    return `${outcomeOutput} <b>It's a draw!</b>`;
+var main = function (input) {
+  var myOutputValue = "";
+  if (currentGameMode == "waiting for user name") {
+    // if the game mode is user name... set the name as the input
+    userName = input;
+    // now that we have the name, switch the mode
+    currentGameMode = "SPS";
+    myOutputValue = "Hello " + userName + ", enter a guess to start the game!";
+    return myOutputValue;
+  } else if (currentGameMode == "SPS") {
+    // if the game mode is SPS game... define playerGuess as the input
+    var playerGuess = input;
+    // then return to SPS game logic
+    var computerGuess = generateGuess();
+    //console.log("This is what the computer played: ", computerGuess);
+    var outcomeOutput = `Your guess: ${playerGuess}. Computer's guess: ${computerGuess}.`;
+    //console.log("this is players guess", playerGuess);
+    countOfRounds += 1;
+    //console.log("this is the count of rounds", countOfRounds);
+    if (computerGuess == playerGuess) {
+      return `<b>It's a draw!</b> <br><br> ${outcomeOutput} <br> You have won ${countOfWins}/${countOfRounds} rounds.`;
+    }
+    // user chooses Scissors AND computer chooses Paper
+    // OR  Paper AND Stone OR Stone and Scissors = WIN
+    if (
+      (playerGuess == "scissors" && computerGuess == "paper") ||
+      (playerGuess == "paper" && computerGuess == "stone") ||
+      (playerGuess == "stone" && computerGuess == "scissors")
+    ) {
+      countOfWins += 1;
+      console.log("this is the count of wins", countOfWins);
+      return `<b>You win!</b>🥳 <br><br> ${outcomeOutput} <br> You have won ${countOfWins}/${countOfRounds} rounds.`;
+    }
+    // user chooses Scissors AND computer chooses Stone
+    // OR  Paper AND Scissors OR Stone AND Paper
+    if (
+      (playerGuess == "scissors" && computerGuess == "stone") ||
+      (playerGuess == "paper" && computerGuess == "scissors") ||
+      (playerGuess == "stone" && computerGuess == "paper")
+    ) {
+      return `<b>You lose</b>🤷 <br><br> ${outcomeOutput} <br> You have won ${countOfWins}/${countOfRounds} rounds.`;
+    }
+    return `Please input a valid guess of scissors, paper or stone!`;
   }
-  // user chooses Scissors AND computer chooses Paper
-  // OR  Paper AND Stone OR Stone and Scissors = WIN
-  if (
-    (playerGuess == "scissors" && computerGuess == "paper") ||
-    (playerGuess == "paper" && computerGuess == "stone") ||
-    (playerGuess == "stone" && computerGuess == "scissors")
-  ) {
-    return `${outcomeOutput} <br><br> <b>You win!</b>`;
-  }
-  // user chooses Scissors AND computer chooses Stone
-  // OR  Paper AND Scissors OR Stone AND Paper
-  if (
-    (playerGuess == "scissors" && computerGuess == "stone") ||
-    (playerGuess == "paper" && computerGuess == "scissors") ||
-    (playerGuess == "stone" && computerGuess == "paper")
-  ) {
-    return `${outcomeOutput} <br><br> <b>You lose</b>!`;
-  }
-  return `Please input a valid guess of scissors, paper or stone!`;
 };
